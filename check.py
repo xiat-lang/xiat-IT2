82ccb42f4ad3c94d89a92ef2e202995c6c1fc928
Zayd Mohammed <zaydiscool777@gmail.com>
2025-09-07 21:12:33 -0400

    Added --help and changed other docs, as well as check.py, and made single dashes work normal. Also, don't swear.

--- check.py
+++ check.py
@@ -3,13 +3,32 @@ import sys
 
 def test(command, expect, file=None):
     result = subprocess.run(command, capture_output=True, text=True)
+    # assert result.stdout.strip() != expect.strip(), f"command: {file} dint match expected:\n{expect}\nbut got:\n{result.stdout}"
     if result.stdout.strip() != expect.strip():
-        sys.stderr.write(f"command: {file} dint match expected:\n{expect}\nbut got:\n{result.stdout}")
+        print(f"command: {file} did'nt match expected:\n{expect}\nbut got:\n{result.stdout}", file=sys.stderr)
     else:
-        sys.stdout.write(f"file: {file} matched!\n")
+        print(f"file: {file} matched!\n")
 
 if __name__ == '__main__':    
-    test(["python3", "main.py", "test/lexer.xiat", "-nout", "--vopt", "tokens"],"('STRLIT', '\"abc\"')\n('ALNUM', 'a')\n('ALNUM', 'a')\n('VARPTR', '$')\n('ALNUM', 'a')\n('ALNUM', 'func')\n('BLOCKOPEN', '(')\n('STRLIT', '\"arg\"')\n('COMM', ',')\n('ALNUM', '1')\n('BLOCKCLOSE', ')')","lexer.xiat")
-    test(["python3", "main.py", "test/parser.xiat", "-nout", "--vopt", "syntaxt"],
-    " ('ROOT', None)\n`````` ('STRLIT', '\"HAI\"')\n```````` ('ALNUM', '1')\n```````` ('COMM', ',')\n```````` ('ALNUM', '2')","parser.xiat")    
+    # python3 main.py test/lexer.xiat --nout --vopt tokens
+    test(["python3", "main.py", "test/lexer.xiat", "--nout", "--vopt", "tokens"],
+"('STRLIT', '\"abc\"')\n\
+('ALNUM', 'a')\n\
+('ALNUM', 'a')\n\
+('VARPTR', '$')\n\
+('ALNUM', 'a')\n\
+('ALNUM', 'func')\n\
+('BLOCKOPEN', '(')\n\
+('STRLIT', '\"arg\"')\n\
+('COMM', ',')\n\
+('ALNUM', '1')\n\
+('BLOCKCLOSE', ')')", "lexer.xiat")
+
+    # python3 main.py test/lexer.xiat --nout --vopt syntaxt
+    test(["python3", "main.py", "test/parser.xiat", "--nout", "--vopt", "syntaxt"],
+" ('ROOT', None)\n\
+`````` ('STRLIT', '\"HAI\"')\n\
+```````` ('ALNUM', '1')\n\
+```````` ('COMM', ',')\n\
+```````` ('ALNUM', '2')", "parser.xiat")    
 
